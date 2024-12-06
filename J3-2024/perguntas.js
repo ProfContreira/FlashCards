a658f423aeefc9b6de13eba70f968f5670888dba
Alexandre Contreira <105686453+ProfContreira@users.noreply.github.com>
2024-12-06 15:48:13 -0300

    Add files via upload

---
+++ J3-2024/perguntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'É uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
